9d74b61e016a1f5c713603bb9b8caf0faa36c8ad
nada1004tj <nada1004tj@naver.com>
2020-04-02 19:40:32 +0900

    new file

---
+++ src/components/Axios.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+
+
+export default {
+  methods: {
+    fetchData: function() {
+        this.$axios.get('https://jsonplaceholder.typicode.com/users/')
+            .then((response) => {
+                console.log(response.data);
+                this.result = response.data
+            })
+    }
+  }
+}
+</script>
---
+++ src/views/PlusLayout/SingleDigit/SingleDigitLayout.vue
@@ -0,0 +1,33 @@
+<<template>
+  <div>
+    <Print></Print>
+    <button @click="showLog">연락처 1건 조회</button>
+    <Axios v-on:show-log="fetchData"></Axios>
+    {{ result }}
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Axios from '@/components/Axios'
+import Print from '@/components/Print'
+
+
+export default {
+  name: 'SingleDigit',
+  data() {
+    return {
+        result: "cc"
+    }
+  },
+  components: {
+    'Print': Print,
+    'Axios': Axios
+  },
+  methods: {
+    showLog:function(){
+      this.$emit('show-log');
+    }
+  }
+}
+</script>
